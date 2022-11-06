7ac7d797ce8cb6e3a599e47a2db47a8c6dfbf6e8
Loshan Sundaramoorthy <losh0001@gmail.com>
2022-11-06 17:12:37 +0100

    Code structure changed according to the given assignment with the method findTime deleted.

--- src/Solver.java
+++ src/Solver.java
@@ -1,35 +1,19 @@
 /**
- * @author Loshan Sundaramoorthy
- * @version 31/10-2022
+ * @author Loshan Sundaramoorthy og Søren Fals Vind
+ * @version 1.0
  */
+
 public class Solver {
+
+
     private int noOfQueens;
     private int[] queens;
     private int noOfSolutions;
-    private boolean showSolutions;
+    private boolean showSolutions = true;
 
-    public void findAllSolutions(int noOfQueens) {
-        System.out.println("********************************************************************");
-        System.out.println("Solutions for " + noOfQueens + " queens:");
-        System.out.println("");
 
-        this.showSolutions = true;
-        long result= findTime(noOfQueens);
-
-        System.out.println("");
-        System.out.println("A total of " + noOfSolutions + " solutions was found in " + result + " ms");
-        System.out.println("********************************************************************");
-    }
-
-    public void findNoOfSolutions(int min, int max) {
-        System.out.println("********************************************************************");
-        System.out.println("  Queens      Solutions      Time(ms)      Solutions/ms             ");
-        this.showSolutions = false;
-        for(int i = min; i <= max; i++) {
-            long result = Math.max(1, findTime(i));
-            System.out.format(" %6d %,12d %,10d %,12d %n", i, noOfSolutions, result, noOfSolutions / result);
-        }
-        System.out.println("********************************************************************");
+    public static void main(String[] args) {
+        testSolver();
     }
 
     public static void testSolver() {
@@ -42,14 +26,68 @@ public class Solver {
         test.findNoOfSolutions(1, 12);
     }
 
-    private long findTime(int noOfQueens) {
+    public Solver() {
+
+    }
+
+    /**
+     * Indsætter N antal dronninger, og printer alle løsningerne ud, samt runtime.
+     * @param noOfQueens
+     */
+
+    public void findAllSolutions(int noOfQueens) {
+        noOfSolutions = 0;
         this.noOfQueens = noOfQueens;
         this.queens = new int[noOfQueens];
-        this.noOfSolutions = 0;
-        long result = System.currentTimeMillis();
+        if(showSolutions) {
+            System.out.println("********************************************************************");
+            System.out.println("Solutions for " + noOfQueens + " queens:");
+            System.out.println("");
+        }
+
+        long before = System.currentTimeMillis();
         positionQueens(0);
-        return System.currentTimeMillis() - result;
+        long result = System.currentTimeMillis() - before;
+
+        if(showSolutions) {
+            System.out.println("");
+            System.out.println("A total of " + noOfSolutions + " solutions was found in " + result + " ms");
+            System.out.println("********************************************************************");
+        }
     }
+
+    /**
+     *
+     * Bruger findAllSolution med et specificeret antal dronninger og konstruere en tabel med løsningerne.
+     *
+     * @param min Min antal dronninger.
+     * @param max Max antal dronninger.
+     */
+
+    public void findNoOfSolutions(int min, int max) {
+        showSolutions = false;
+
+        System.out.println("********************************************************************");
+        System.out.println("  Queens      Solutions      Time(ms)      Solutions/ms             ");
+        for(int i = min; i <= max; i++) {
+
+            long before = System.currentTimeMillis();
+            findAllSolutions(i);
+            long result = System.currentTimeMillis() - before;
+
+            System.out.format(" %6d %,12d %,10d %,12d %n", i, noOfSolutions, result+1, noOfSolutions / (result+1));
+        }
+        System.out.println("********************************************************************");
+        System.out.println();
+
+        showSolutions = true;
+    }
+
+    /**
+     * Placere en dronning i den specificerede række.
+     *
+     * @param row rækken dronningen placeres i.
+     */
 
     private void positionQueens(int row) {
         if(row == noOfQueens) {
@@ -67,11 +105,16 @@ public class Solver {
         }
     }
 
+    /**
+     * Tjekker om den nuværende position af dronningen er lovlig.
+     *
+     * @param row Rækken der tjekkes
+     * @param col Kolonnen der tjekkes
+     * @return booelean.
+     */
+
     private boolean legal(int row, int col) {
         for(int i = 0; i < row; i++) {
-            if(i == row) {
-                continue;
-            }
             int abs = Math.abs(row - i);
             int j = queens[i];
             if(j == col || col == (j + abs) || col == (j - abs)) {
@@ -81,6 +124,12 @@ public class Solver {
         return true;
     }
 
+    /**
+     *
+     * Printer en løsning.
+     *
+     */
+
     private void printSolution() {
         String result = new String();
         for(int i = 0; i < noOfQueens; i++){
@@ -88,6 +137,14 @@ public class Solver {
         }
         System.out.println(result);
     }
+
+    /**
+     * Metoden bruges til at konvertere row og col til den almen skak notation
+     *
+     * @param row nuværende række
+     * @param col Nuværende kolonne
+     * @return Returner en string.
+     */
 
     private String convert(int row, int col) {
         char ch = (char) ('a' + col);
